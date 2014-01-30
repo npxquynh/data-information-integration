98ed00c4bdfefc6f62c9b5e56d5cfcc01c06c723
Quynh Nguyen <quynh.xq@gmail.com>
2014-01-30 12:52:21 +0100

    Add some more fields into HBase table

--- dump_data/test_thrift.py
+++ dump_data/test_thrift.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import logging
+import sys
 import pdb
 
 from thrift.transport import TSocket
@@ -8,7 +9,7 @@ from thrift.protocol import TBinaryProtocol
 from thrift.transport import TTransport
 from hbase import Hbase
 
-DEFAULT_TABLE_NAME = 'githubarchive'
+DEFAULT_TABLE_NAME = 'github_events'
 DEFAULT_HOST = 'localhost'
 DEFAULT_PORT = 9090
 
@@ -54,56 +55,81 @@ if __name__ == '__main__':
             Hbase.ColumnDescriptor(name='created_at'),
             Hbase.ColumnDescriptor(name='actor'),
             Hbase.ColumnDescriptor(name='repository'),
-            Hbase.ColumnDescriptor(name='type')
+            Hbase.ColumnDescriptor(name='event')
         ])
 
     # parse JSON
-    line = '{"created_at":"2013-09-20T09:03:41-07:00","payload":{"shas":[["7af40c10725e4fe8a5b593ef887bc5757f3e7495","inodetelic@gmail.com","Added year check for today date","inod",true],["dea687bf8213acf25ccc05c5122166e02ac5022d","donatj@gmail.com","Merge pull request #2 from inod/patch-1Added year check for today date","Jesse Donat",true]],"size":2,"ref":"refs/heads/master","head":"dea687bf8213acf25ccc05c5122166e02ac5022d"},"public":true,"type":"PushEvent","url":"https://github.com/donatj/SimpleCalendar/compare/bcdd0b6799...dea687bf82","actor":"donatj","actor_attributes":{"login":"donatj","type":"User","gravatar_id":"50cb4981bbaa51b1713269a29f78d826","name":"Jesse Donat","company":"Donat Studios","blog":"http://donatstudios.com","location":"Minnesota","email":"donatj@gmail.com"},"repository":{"id":749771,"name":"SimpleCalendar","url":"https://github.com/donatj/SimpleCalendar","description":"A simple PHP calendar rendering class","homepage":"http://donatstudios.com","watchers":13,"stargazers":13,"forks":8,"fork":false,"size":189,"owner":"donatj","private":false,"open_issues":1,"has_issues":true,"has_downloads":true,"has_wiki":true,"language":"PHP","created_at":"2010-06-30T12:38:08-07:00","pushed_at":"2013-09-20T09:03:40-07:00","master_branch":"master"}}'
-    try:
-        event = json.loads(line)
-    except:
-        logging.warn("Failed on line")
-        # continue
-        pass
+    # line = '{"created_at":"2013-09-20T10:00:06-07:00","payload":{"pages":[{"page_name":"Home","title":"Home","summary":null,"action":"created","sha":"fbc868e990ef516a0be2a5c87b17228a75eb655f","html_url":"https://github.com/iSamuraii/FFReader/wiki/Home"}]},"public":true,"type":"GollumEvent","url":"https://github.com/iSamuraii/FFReader/wiki/Home","actor":"iSamuraii","actor_attributes":{"login":"iSamuraii","type":"User","gravatar_id":"65fbae24a4f3ba884f52bff624b62985"},"repository":{"id":12917102,"name":"FFReader","url":"https://github.com/iSamuraii/FFReader","description":"An API based reader for Forumfree","watchers":0,"stargazers":0,"forks":0,"fork":false,"size":268,"owner":"iSamuraii","private":false,"open_issues":0,"has_issues":true,"has_downloads":true,"has_wiki":true,"language":"PHP","created_at":"2013-09-18T00:44:20-07:00","pushed_at":"2013-09-20T09:59:14-07:00","master_branch":"master"}}'
+    for line in sys.stdin:
+    # pdb.set_trace()
+        line = line.decode('ascii', 'ignore')
+        # encoding = chardet.detect(line)
+        # print encoding['encoding']
+        # if encoding['encoding'] != 'ascii':
+        #     pass
+        #     # continue
 
-    try:
-        mutations = []
+        try:
+            event = json.loads(line)
+        except:
+            logging.warn("Failed parsing json on line")
+            continue
+            # pass
 
-        # Add data to 'column families' created_at
-        temp = event['created_at'][:-6] # remove 5 character at the end of string
-        created_at = datetime.datetime.strptime(temp,
-            "%Y-%m-%dT%H:%M:%S")
+        try:
+            mutations = []
 
-        # Add data to 'column families' type
-        event_type = event['type']
+            # Add data to 'column families' type
+            event_type = event['type']
 
-        # Add data to 'column families' actor
-        actor = event['actor_attributes']
+            if event_type not in ['CommitCommentEvent',
+                'IssueCommentEvent',
+                'IssuesEvent',
+                'PublicEvent',
+                'PushEvent',
+                'PullRequestEvent',
+                'PullRequestReviewCommentEvent',
+                'ForkApplyEvent']:
+                continue
 
-        # Add data to 'column families' repository
-        repo = event['repository']
+            # Add data to 'column families' created_at
+            print event[-6:]
+            temp = event['created_at'][:-6] # remove 5 character at the end of string
+            created_at = datetime.datetime.strptime(temp,
+                "%Y-%m-%dT%H:%M:%S")
 
-        # Define rowkey
-        # pdb.set_trace()
-        rowkey = "%d%d%d%s" % (created_at.year, created_at.month, created_at.day, actor['login'])
+            # Add data to 'column families' actor
+            actor = event['actor_attributes']
 
-        mutations = [
-            Hbase.Mutation(column='created_at:year', value=str(created_at.year)),
-            Hbase.Mutation(column='created_at:month', value=str(created_at.month)),
-            Hbase.Mutation(column='created_at:day', value=str(created_at.day)),
-            Hbase.Mutation(column='type:name', value=str(event_type)),
-            Hbase.Mutation(column='actor:login', value=actor['login']),
-            Hbase.Mutation(column='actor:name', value=actor['name']),
-            Hbase.Mutation(column='repository:id', value=str(repo['id'])),
-            Hbase.Mutation(column='repository:url', value=repo['url']),
-            Hbase.Mutation(column='repository:watchers', value=str(repo['watchers'])),
-            Hbase.Mutation(column='repository:stargazers', value=str(repo['stargazers'])),
-            Hbase.Mutation(column='repository:forks', value=str(repo['forks'])),
-            Hbase.Mutation(column='repository:language', value=repo['language'])
-        ]
+            # Add data to 'column families' repository
+            repo = event['repository']
 
-        th.reconnect()
-        th.client.mutateRow(DEFAULT_TABLE_NAME, 'rowkey', mutations, {})
-    except KeyError:
-        logging.warn("Key not found: %s" % key)
+            # Define rowkey
+            # pdb.set_trace()
+            rowkey = "%d%d%d_%s" % (created_at.year, created_at.month, created_at.day, actor['login'])
+
+            mutations = [
+                Hbase.Mutation(column='created_at:year', value=str(created_at.year)),
+                Hbase.Mutation(column='created_at:month', value=str(created_at.month)),
+                Hbase.Mutation(column='created_at:day', value=str(created_at.day)),
+                Hbase.Mutation(column='created_at:weekday', value=str(created_at.weekday())),
+                Hbase.Mutation(column='created_at:hour', value=str(created_at.hour)),
+                Hbase.Mutation(column='created_at:timestamp', value=temp),
+                Hbase.Mutation(column='event:type', value=str(event_type)),
+                Hbase.Mutation(column='actor:login', value=actor['login']),
+                Hbase.Mutation(column='actor:name', value=actor.get('name', '')),
+                Hbase.Mutation(column='repository:id', value=str(repo['id'])),
+                Hbase.Mutation(column='repository:name', value=str(repo.get('name',''))),
+                Hbase.Mutation(column='repository:url', value=repo['url']),
+                Hbase.Mutation(column='repository:watchers', value=str(repo['watchers'])),
+                Hbase.Mutation(column='repository:stargazers', value=str(repo['stargazers'])),
+                Hbase.Mutation(column='repository:forks', value=str(repo['forks'])),
+                Hbase.Mutation(column='repository:language', value=repo.get('language', ''))
+            ]
+
+            th.reconnect()
+            th.client.mutateRow(DEFAULT_TABLE_NAME, rowkey, mutations, {})
+        except KeyError as e:
+            # pdb.set_trace()
+            logging.warn("Key not found: %s for event type: %s" % (e, event.get('type', '')))
 
