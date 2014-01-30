0df188e2234c512e489fdd88a2e5c77fde07aeb4
Quynh Nguyen <quynh.xq@gmail.com>
2014-01-30 12:58:25 +0100

    Script to import json 10 files at a time.

---
+++ scripts/import_json.sh
@@ -0,0 +1,10 @@
+cd /media/quynh/DATA/Courses/DII/github\ data
+
+# Move the first 10 files to different folder
+ls | head -10 | while read file; do mv $file "./processing/$file"; done
+
+# Extract
+
+# Calling python
+cd /home/quynh/code/data-information-integration/dump_data
+python test_thrift.py < /media/quynh/DATA/Courses/DII/github\ data/processing/2013*.json
