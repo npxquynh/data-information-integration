12e5c3a920c506aa6f0b4938847ff2dd8d4fefa2
duyvk <duyvk142@gmail.com>
2014-02-17 21:49:52 +0100

    add daily command

--- duyvk/workflow.sh
+++ duyvk/workflow.sh
@@ -21,6 +21,8 @@ TBLPROPERTIES ('hbase.table.name' = 'lang_emotion');
 
 
 
+
+
 // query
 
 //----------- group by created_at_hour-------------------
@@ -36,3 +38,93 @@ select actor_login , repository_name , count(*) from github_events_hive group by
 select actor_login , repository_language , count(*) from github_events_hive group by actor_login, repository_language;
 
 
+
+
+
+
+
+
+
+------------------------DAILY-----------------------
+
+
+
+
+// create a daily table in hive
+CREATE  TABLE  github_events_hive_daily (rowkey STRING,actor_login STRING,actor_name STRING,repository_id STRING,repository_name STRING,repository_url STRING,repository_watchers INT,repository_stargazers INT,repository_forks INT,repository_language STRING,created_at_weekday INT,created_at_hour INT,created_at_timestamp TIMESTAMP,event_type STRING, event_message STRING)
+PARTITIONED by (created_at_year INT,created_at_month INT,created_at_day INT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+
+
+// insert data from "github_events_hive" to "github_events_hive_daily"
+INSERT OVERWRITE TABLE github_events_hive_daily PARTITION (created_at_year=2014,created_at_month=2,created_at_day=1)
+SELECT rowkey ,actor_login ,actor_name ,repository_id ,repository_name ,repository_url ,repository_watchers ,repository_stargazers ,repository_forks ,repository_language ,created_at_weekday ,created_at_hour ,created_at_timestamp ,event_type , event_message  FROM github_events_hive WHERE created_at_year=2014 and created_at_month=2 and created_at_day=1;
+
+//query
+select actor_login , created_at_hour , count(*) from github_events_hive_daily group by actor_login, created_at_hour;
+select actor_login , created_at_weekday , count(*) from github_events_hive_daily group by actor_login, created_at_weekday;
+select actor_login , event_type , count(*) from github_events_hive_daily group by actor_login, event_type;
+select actor_login , repository_language , count(*) from github_events_hive_daily group by actor_login, repository_language;
+select actor_login , repository_id , count(*) from github_events_hive_daily group by actor_login, repository_id;
+
+
+// top 20 most active user
+select actor_login , count(*) as events from github_events_hive_daily group by actor_login sort by events desc limit 20;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------ignore this ----------------------------------
+
+CREATE EXTERNAL TABLE  test_hive (rowkey STRING,actor_login STRING,actor_name STRING,repository_id STRING,repository_name STRING,repository_url STRING,repository_watchers INT,repository_stargazers INT,repository_forks INT,repository_language STRING,created_at_year INT,created_at_month INT,created_at_day INT,created_at_weekday INT,created_at_hour INT,created_at_timestamp TIMESTAMP,event_type STRING, event_message STRING)
+STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+WITH SERDEPROPERTIES ('hbase.columns.mapping' = ':key,actor:login,actor:name,repository:id,repository:name,repository:url,repository:watchers,repository:stargazers,repository:forks,repository:language,created_at:year,created_at:month,created_at:day,created_at:weekday,created_at:hour,created_at:timestamp,event:type,event:message') 
+TBLPROPERTIES ('hbase.table.name' = 'test'); 
+
+
+CREATE  TABLE  test_hive_daily (rowkey STRING,actor_login STRING,actor_name STRING,repository_id STRING,repository_name STRING,repository_url STRING,repository_watchers INT,repository_stargazers INT,repository_forks INT,repository_language STRING,created_at_weekday INT,created_at_hour INT,created_at_timestamp TIMESTAMP,event_type STRING, event_message STRING)
+PARTITIONED by (created_at_year INT,created_at_month INT,created_at_day INT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+
+
+INSERT OVERWRITE TABLE test_hive_daily PARTITION (created_at_year=2014,created_at_month=2,created_at_day=1)
+SELECT rowkey ,actor_login ,actor_name ,repository_id ,repository_name ,repository_url ,repository_watchers ,repository_stargazers ,repository_forks ,repository_language ,created_at_weekday ,created_at_hour ,created_at_timestamp ,event_type , event_message  FROM test_hive WHERE created_at_year=2014 and created_at_month=2 and created_at_day=1;
+
+select actor_login , created_at_hour , count(*) from test_hive_daily group by actor_login, created_at_hour;
+select actor_login , created_at_weekday , count(*) from test_hive_daily group by actor_login, created_at_weekday;
+select actor_login , event_type , count(*) from test_hive_daily group by actor_login, event_type;
+select actor_login , repository_language , count(*) from test_hive_daily group by actor_login, repository_language;
+select actor_login , repository_id , count(*) from test_hive_daily group by actor_login, repository_id;
+
+
+
+select actor_login , count(*) as events from test_hive_daily group by actor_login sort by events desc limit 20;
